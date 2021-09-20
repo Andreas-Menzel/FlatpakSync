f6fc6c68faa9b48d47d898e8bc32ee9dce358ade
Andreas Menzel <mail@andreas-menzel.com>
2021-09-20 16:03:32 +0200

    Removed most verbosity

--- FlatpakSync.py
+++ FlatpakSync.py
@@ -14,9 +14,6 @@ parser.add_argument('-b', '--backup',
 parser.add_argument('-i', '--install',
     action='store_true',
     help='Install all Flatpak.')
-parser.add_argument('-v', '--verbose',
-    action='store_true',
-    help='Display info messages.')
 parser.add_argument('-f', '--file',
     default="FlatpakSync__installed_apps.txt",
     help='')
@@ -26,12 +23,8 @@ args = parser.parse_args()
 
 
 def get_installed_apps():
-    if args.verbose:
-        print("[INFO] Checking file for apps...")
-
     if not os.path.isfile(args.file):
-        if args.verbose:
-            print("[INFO] File ", args.file, "does not exist. No apps found.")
+        print('File', '"' + args.file + '"', 'does not exist.')
         return []
 
     return_string = ""
@@ -48,25 +41,13 @@ def get_installed_apps():
 
 
 def create_backup():
-    if args.verbose:
-        print("[INFO] Creating backup...")
-
     with open(args.file, 'w') as file:
         subprocess.run(["flatpak", "list", "--columns=origin,application,arch,branch"], stdout=file, text=True).stdout
 
-    if args.verbose:
-        print("[INFO] Finished creating backup.")
-
 
 def install(apps):
-    if args.verbose:
-        print("[INFO] Installing apps...")
-
     for app in apps:
         subprocess.run(["flatpak", "install", "--noninteractive", "-y", app[0], app[1] + "/" + app[2] + "/" + app[3]])
-
-    if args.verbose:
-        print("[INFO] Finished installing apps.")
 
 
 if __name__ == '__main__':
